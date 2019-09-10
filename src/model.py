58946b9002d6f2f8baa354fef5f79373f4189bc4
Peter Siemen <peter.siemen@googlemail.com>
2019-09-10 12:29:59 +0200

    decoderrnn .. wip

--- src/model.py
+++ src/model.py
@@ -9,7 +9,7 @@ class EncoderCNN(nn.Module):
         resnet = models.resnet50(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
-        
+
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
@@ -19,12 +19,24 @@ class EncoderCNN(nn.Module):
         features = features.view(features.size(0), -1)
         features = self.embed(features)
         return features
-    
+
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1, dropout=False, output_size=1024):
+        super(DecoderRNN, self).__init__()
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+
+        # define model layers
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers,
+                            dropout=dropout, batch_first=True)
+
+        self.fc = nn.Linear(hidden_size, output_size)
+        self.log_softmax = nn.LogSoftmax(dim=1)
+
     def forward(self, features, captions):
         pass
 
